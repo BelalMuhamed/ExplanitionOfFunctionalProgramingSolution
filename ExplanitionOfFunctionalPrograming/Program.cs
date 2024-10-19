3955843caa9f5ecae78258873505a9b3dbb69c8a
BelalBasal <basdibrahim@gmail.com>
2024-10-20 01:00:55 +0300

    Add project files.

---
+++ ExplanitionOfFunctionalPrograming/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExplanitionOfFunctionalPrograming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
