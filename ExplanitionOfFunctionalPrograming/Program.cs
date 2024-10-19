7426a8d70de95888e749bc4a5ef1d752b43221a5
BelalBasal <basdibrahim@gmail.com>
2024-10-20 01:55:09 +0300

    testing the code

--- ExplanitionOfFunctionalPrograming/Program.cs
+++ ExplanitionOfFunctionalPrograming/Program.cs
@@ -1,4 +1,5 @@
-﻿using ExplanitionOfFunctionalPrograming.Helper;
+﻿using ExplanitionOfFunctionalPrograming.Entities;
+using ExplanitionOfFunctionalPrograming.Helper;
 
 namespace ExplanitionOfFunctionalPrograming
 {
@@ -6,10 +7,22 @@ namespace ExplanitionOfFunctionalPrograming
     {
         static void Main(string[] args)
         {
-            
 
-           
+            ConceptOfFunctionalProgrammingAndPureFunction();
+
+
         }
-
+        private static void ConceptOfFunctionalProgrammingAndPureFunction() 
+        {
+            var Employees = GetAllEmployee.LoadEmployees();
+            var q01 = Employees.Filter(e => e.FirstName.ToLowerInvariant() == "ma");
+            q01.Print("Employee first name start with \'ma\'");
+            var q02 = Employees.Filter(e => e.Department.ToLowerInvariant() == "hr");
+            q02.Print("Employee first name start with \'hr\'");
+            var q03 = Employees.Filter(e => e.HireDate.Year == 2018);
+            q03.Print("Employees there hiring date in 2018");
+            var q04 = Employees.Filter(e => e.Salary == 10700);
+            q04.Print("Employees with salary == 10700");
+        }
     }
 }
